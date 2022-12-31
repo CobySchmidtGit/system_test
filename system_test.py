93d37df22c16ed7a5a99e1dd20920f6f5c5fd772
coby <cobys.schmidt@gmail.com>
2022-12-31 22:35:55 +0200

    reading yaml

--- system_test.py
+++ system_test.py
@@ -1,3 +1,4 @@
+import errno
 import yaml
 import sys
 
@@ -9,3 +10,16 @@ if (n_args==1):
     sys.exit(1)
 
 
+file_name = sys.argv[1]
+
+try:
+    fileb = open(file_name, 'r')
+except IOError as e:
+    print("could not open file", file_name,  ":", e.strerror, file=sys.stderr)
+    sys.exit(1)
+
+
+yaml_data=yaml.load(fileb)
+for BOM in yaml_data:
+        for dir in BOM:
+                print (dir)
